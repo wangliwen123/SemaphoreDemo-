b9c17f6dd0d9d0fda1c8597da406ad189d8fcdd1
wangliwen123 <46805213+wangliwen123@users.noreply.github.com>
2019-02-20 10:46:24 +0800

    Add files via upload

---
+++ SemaphoreDemo.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+public class SemaphoreDemo {
+
+    public static void main(String[] args) {
+
+        Semaphore semaphore = new Semaphore(3);
+        for (int i = 1; i <=6; i++)
+            new Thread(() -> {
+                try {
+                    semaphore.acquire();
+                    System.out.println(Thread.currentThread().getName() + "已经抢到了");
+                    TimeUnit.SECONDS.sleep(3);
+                    System.out.println(Thread.currentThread().getName() + "离开了车位");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }finally{
+                    semaphore.release();
+                }
+
+            }, String.valueOf(i)).start();
+    }
+}
